1b33bbc4f13b5c32b3adc916d80753dbe64e47cb
Ivan <isnob46@gmail.com>
2020-08-09 18:21:25 +0200

    add buff to server

--- remote_backup_server/src/main.cpp
+++ remote_backup_server/src/main.cpp
@@ -15,6 +15,7 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/asio.hpp>
+#include <boost/array.hpp>
 
 using boost::asio::ip::tcp;
 
@@ -24,9 +25,10 @@ std::string make_daytime_string()
     time_t now = time(0);
     return ctime(&now);
 }
+// We will use shared_ptr and enable_shared_from_this because
+// we want to keep the tcp_connection object alive as long as there is an operation that refers to it.
 
-class tcp_connection
-    : public boost::enable_shared_from_this<tcp_connection>
+class tcp_connection : public boost::enable_shared_from_this<tcp_connection>
 {
 public:
     typedef boost::shared_ptr<tcp_connection> pointer;
@@ -41,15 +43,36 @@ public:
     {
         return socket_;
     }
-
+    // In the function start(), we call boost::asio::async_write() to serve the data to the client. Note that we are using boost::asio::async_write(),
+    // rather than ip::tcp::socket::async_write_some(), to ensure that the entire block of data is sent.
     void start()
     {
         message_ = make_daytime_string();
-        std::cout << std::this_thread::get_id() << " START WRITE " << message_ << std::endl;
-        boost::asio::async_write(socket_, boost::asio::buffer(message_),
+        // std::cout << std::this_thread::get_id() << " START WRITE " << message_ << std::endl;
+        /* boost::asio::async_write(socket_, boost::asio::buffer(message_),
                                  boost::bind(&tcp_connection::handle_write, shared_from_this(),
                                              boost::asio::placeholders::error,
-                                             boost::asio::placeholders::bytes_transferred));
+                                             boost::asio::placeholders::bytes_transferred)); */
+
+        boost::asio::async_read(socket_, boost::asio::buffer(buf), boost::bind(&tcp_connection::handler, shared_from_this(), boost::asio::placeholders::error));
+    }
+
+    void handler(
+        const boost::system::error_code &error // Result of operation.
+
+        /* std::size_t bytes_transferred */) // Number of bytes copied into the
+                                             // buffers. If an error occurred,
+                                             // this will be the  number of
+                                             // bytes successfully transferred
+                                             // prior to the error.
+    {
+        std::string s = socket().remote_endpoint().address().to_string();
+
+        std::cout << "REQ FROM  " << s << std::endl;
+        if (!error)
+        {
+            std::cout << "RECEIVED " << buf.data() << std::endl;
+        };
     }
 
 private:
@@ -62,6 +85,8 @@ private:
                       size_t /*bytes_transferred*/)
     {
     }
+
+    boost::array<char, 1> buf;
 
     tcp::socket socket_;
     std::string message_;
@@ -78,15 +103,17 @@ public:
     }
 
 private:
+    // creates a socket and initiates an asynchronous accept operation to wait for a new connection.
     void start_accept()
     {
         tcp_connection::pointer new_connection = tcp_connection::create(io_context_);
 
         acceptor_.async_accept(new_connection->socket(),
-                               boost::bind(&tcp_server::handle_accept, this, new_connection,
-                                           boost::asio::placeholders::error));
+                               boost::bind(&tcp_server::handle_accept, this, new_connection, boost::asio::placeholders::error));
     }
 
+    // The function handle_accept() is called when the asynchronous accept operation initiated by start_accept() finishes.
+    // It services the client request, and then calls start_accept() to initiate the next accept operation.
     void handle_accept(tcp_connection::pointer new_connection,
                        const boost::system::error_code &error)
     {
